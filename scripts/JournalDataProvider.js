020d888531e753d7c42a3a78b69fbf0e3cf4002c
Abdurehman05 <abdukr5@gmail.com>
2020-09-04 16:26:55 -0500

    api entries created

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -7,37 +7,6 @@
  */
 
 // This is the original data.
-const journal = [{
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-
-    {
-        id: 2,
-        date: "07/26/2025",
-        concept: "Complex Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 3,
-        date: "07/27/2025",
-        concept: "Git and GitHub",
-        entry: "I learned about git,which is a version conrol system and developers collaboration tool. I cloned, pulled, pushed,merged and created branches.",
-        mood: "Content"
-    },
-    {
-        id: 4,
-        date: "07/28/2025",
-        concept: "Automate html representation",
-        entry: "Using js components and modules, we created html representation by manpulating the DOM representation.",
-        mood: "Stressed"
-    }
-]
-
 /*
     You export a function that provides a version of the
     raw data in the format that you want
